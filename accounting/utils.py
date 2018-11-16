7cc01ba00c5e123f92707dd5cee4e5385c82e4e1
Yovarni Yearwood <yovarniyearwood@gmail.com>
2018-11-16 15:09:08 -0500

    1. Added whitespace in make_invoices()
    Whitespaces makes the code look so much less clustered and improves readability, in my opinion

--- accounting/utils.py
+++ accounting/utils.py
@@ -96,6 +96,7 @@ class PolicyAccounting(object):
         billing_schedules = {'Annual': None, 'Semi-Annual': 3, 'Quarterly': 4, 'Monthly': 12}
 
         invoices = []
+
         first_invoice = Invoice(self.policy.id,
                                 self.policy.effective_date, #bill_date
                                 self.policy.effective_date + relativedelta(months=1), #due
@@ -107,42 +108,55 @@ class PolicyAccounting(object):
             pass
         elif self.policy.billing_schedule == "Two-Pay":
             first_invoice.amount_due = first_invoice.amount_due / billing_schedules.get(self.policy.billing_schedule)
+
             for i in range(1, billing_schedules.get(self.policy.billing_schedule)):
                 months_after_eff_date = i*6
+
                 bill_date = self.policy.effective_date + relativedelta(months=months_after_eff_date)
+
                 invoice = Invoice(self.policy.id,
                                   bill_date,
                                   bill_date + relativedelta(months=1),
                                   bill_date + relativedelta(months=1, days=14),
                                   self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule))
+
                 invoices.append(invoice)
         elif self.policy.billing_schedule == "Quarterly":
             first_invoice.amount_due = first_invoice.amount_due / billing_schedules.get(self.policy.billing_schedule)
+
             for i in range(1, billing_schedules.get(self.policy.billing_schedule)):
                 months_after_eff_date = i*3
+
                 bill_date = self.policy.effective_date + relativedelta(months=months_after_eff_date)
+
                 invoice = Invoice(self.policy.id,
                                   bill_date,
                                   bill_date + relativedelta(months=1),
                                   bill_date + relativedelta(months=1, days=14),
                                   self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule))
+
                 invoices.append(invoice)
         elif self.policy.billing_schedule == "Monthly":
             first_invoice.amount_due = first_invoice.amount_due / billing_schedules.get(self.policy.billing_schedule)
+
             for i in range(1, billing_schedules.get(self.policy.billing_schedule)):
                 months_after_eff_date = i
+
                 bill_date = self.policy.effective_date + relativedelta(months=months_after_eff_date)
+
                 invoice = Invoice(self.policy.id,
                                   bill_date,
                                   bill_date + relativedelta(months=1),
                                   bill_date + relativedelta(months=1, days=14),
                                   self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule))
+
                 invoices.append(invoice)
         else:
             print "You have chosen a bad billing schedule."
 
         for invoice in invoices:
             db.session.add(invoice)
+            
         db.session.commit()
 
 ################################
