cdef1c4ed88641e871d9a5682e268908ad4eb01e
Yovarni Yearwood <yovarniyearwood@gmail.com>
2018-11-27 15:26:03 -0500

    4. Added comments and logging
    Also added logging to the test method to see what was happening where

--- accounting/tests.py
+++ accounting/tests.py
@@ -18,6 +18,7 @@ class TestBillingSchedules(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        print "Setting up TestBillingSchedules class..."
         cls.test_agent = Contact('Test Agent', 'Agent')
         cls.test_insured = Contact('Test Insured', 'Named Insured')
         db.session.add(cls.test_agent)
@@ -29,23 +30,29 @@ class TestBillingSchedules(unittest.TestCase):
         cls.policy.named_insured = cls.test_insured.id
         cls.policy.agent = cls.test_agent.id
         db.session.commit()
+        print "Class set up!\n"
 
     @classmethod
     def tearDownClass(cls):
+        print "Tearing down class..."
         db.session.delete(cls.test_insured)
         db.session.delete(cls.test_agent)
         db.session.delete(cls.policy)
         db.session.commit()
+        print "Class torn down!\n"
 
     def setUp(self):
         pass
 
     def tearDown(self):
+        print "Tearing down..."
         for invoice in self.policy.invoices:
             db.session.delete(invoice)
         db.session.commit()
+        print "Torn down!\n"
 
     def test_annual_billing_schedule(self):
+        print "Testing annual billing schedule...\n"
         self.policy.billing_schedule = "Annual"
         #No invoices currently exist
         self.assertFalse(self.policy.invoices)
@@ -53,20 +60,23 @@ class TestBillingSchedules(unittest.TestCase):
         pa = PolicyAccounting(self.policy.id)
         self.assertEquals(len(self.policy.invoices), 1)
         self.assertEquals(self.policy.invoices[0].amount_due, self.policy.annual_premium)
+        print "Done!\n"
 
     def test_monthly_billing_schedule(self):
+        print "Test monthly billing schedule...\n"
         self.policy.billing_schedule = "Monthly"
         self.assertFalse(self.policy.invoices)
         pa = PolicyAccounting(self.policy.id)
         self.assertEquals(len(self.policy.invoices), 12)
         self.assertEquals(self.policy.invoices[0].amount_due, self.policy.annual_premium/12)
+        print "Done!\n"
 
 
 class TestReturnAccountBalance(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # print("Setting up class...")
+        print "Setting up TestReturnAccountBalance class..."
         cls.test_agent = Contact('Test Agent', 'Agent')
         cls.test_insured = Contact('Test Insured', 'Named Insured')
         db.session.add(cls.test_agent)
@@ -78,49 +88,56 @@ class TestReturnAccountBalance(unittest.TestCase):
         cls.policy.agent = cls.test_agent.id
         db.session.add(cls.policy)
         db.session.commit()
-        # print("Class set up!\n")
+        print "Class set up!\n"
 
     @classmethod
     def tearDownClass(cls):
-        # print("Class tearing down...")
+        print "Tearing down class..."
         db.session.delete(cls.test_insured)
         db.session.delete(cls.test_agent)
         db.session.delete(cls.policy)
         db.session.commit()
-        # print("Class torn down!")
+        print "Class torn down!\n"
 
     def setUp(self):
-        # print("Setting up...")
+        print "Setting up..."
         self.payments = []
-        # print("Set up!")
+        print "Set up!\n"
 
     def tearDown(self):
-        # print("Tearing down...")
+        print "Tearing down..."
         for invoice in self.policy.invoices:
             db.session.delete(invoice)
         for payment in self.payments:
             db.session.delete(payment)
         db.session.commit()
-        # print("Torn down!\n")
+        print "Torn down!\n"
 
     def test_annual_on_eff_date(self):
+        print "Testing account balance on annual effective date...\n"
         self.policy.billing_schedule = "Annual"
         pa = PolicyAccounting(self.policy.id)
         self.assertEquals(pa.return_account_balance(date_cursor=self.policy.effective_date), 1200)
+        print "Done!\n"
 
     def test_quarterly_on_eff_date(self):
+        print "Testing account balance on quarterly effective date...\n"
         self.policy.billing_schedule = "Quarterly"
         pa = PolicyAccounting(self.policy.id)
         self.assertEquals(pa.return_account_balance(date_cursor=self.policy.effective_date), 300)
+        print "Done!\n"
 
     def test_quarterly_on_last_installment_bill_date(self):
+        print "Testing account balance on last installment of quarterly bill date...\n"
         self.policy.billing_schedule = "Quarterly"
         pa = PolicyAccounting(self.policy.id)
         invoices = Invoice.query.filter_by(policy_id=self.policy.id)\
                                 .order_by(Invoice.bill_date).all()
         self.assertEquals(pa.return_account_balance(date_cursor=invoices[3].bill_date), 1200)
+        print "Done!\n"
 
     def test_quarterly_on_second_installment_bill_date_with_full_payment(self):
+        print "Testing account balance on second-to-last installment of quarterly bill date with full payment...\n"
         self.policy.billing_schedule = "Quarterly"
         pa = PolicyAccounting(self.policy.id)
         invoices = Invoice.query.filter_by(policy_id=self.policy.id)\
@@ -128,3 +145,4 @@ class TestReturnAccountBalance(unittest.TestCase):
         self.payments.append(pa.make_payment(contact_id=self.policy.named_insured,
                                              date_cursor=invoices[1].bill_date, amount=600))
         self.assertEquals(pa.return_account_balance(date_cursor=invoices[1].bill_date), 0)
+        print "Done!\n"
--- accounting/utils.py
+++ accounting/utils.py
@@ -17,64 +17,88 @@ class PolicyAccounting(object):
      Each policy has its own instance of accounting.
     """
 
-    def __init__(self, policy_id):
+    def __init__(self, policy_id): # Executed on class initiailization, setting the global class policy and creating invoices if there are none
+        print "Initializing PolicyAccouting class..."
         self.policy = Policy.query.filter_by(id=policy_id).one()
 
         if not self.policy.invoices:
+            print "No invoices are made. Making invoices..."
             self.make_invoices()
+            print "Invoices made!"
+        else:
+            print "Invoices are already made."
+        
+        print "PolicyAccouting intialized!\n"
 
-    def return_account_balance(self, date_cursor=None):
+    def return_account_balance(self, date_cursor=None): # Take in a date and return the corresponding account balance according to invoices and payments
         billing_schedules = {'Annual': 1, 'Semi-Annual': 3, 'Quarterly': 4, 'Monthly': 12}
 
-        print("Billing Schedule: ", self.policy.billing_schedule, billing_schedules.get(self.policy.billing_schedule))
-        print("Billing Date: ", date_cursor)
-
-        if not date_cursor:
+        if not date_cursor: # If date_cursor is equal to the default value (None) or null, set it equal to the current date
+            print "No date has been set. Setting date to current date."
             date_cursor = datetime.now().date()
 
+        # Access the database to grab invoices according to the current policy. Filter them by looking for all invoices whose billing date is on or before 
+        # the date_cursor, then order them by the billing date. The .all() method returns all matched invoices.
         invoices = Invoice.query.filter_by(policy_id=self.policy.id)\
                                 .filter(Invoice.bill_date <= date_cursor)\
                                 .order_by(Invoice.bill_date)\
                                 .all()
 
-        due_now = 0
+        due_now = 0 # Set our due_now variable to 0 as it is to be modified by invoices and payments
 
         if(len(invoices) != 0): # If invoices is empty, don't bother executing code on it as we are wasting time
-            for invoice in invoices:
-                due_now += invoice.amount_due
-                print("Invoice: ", invoice.amount_due)
+            if(len(invoices) == 1):
+                print "1 invoice is present. Listing..."
+                print "The current invoice has a balance due of $", invoices[0].amount_due
+                due_now = invoices[0].amount_due
+            else:
+                print len(invoices), "invoices are present. Listing..."
+                for invoice in invoices:
+                    print "The current invoice has a balance due of $", invoice.amount_due
+                    due_now += invoice.amount_due # Increase due_now for each invoice found
 
+            print "The current amount due is $", due_now
+        else:
+            print "No invoices are present."
+
+        # Same logic as invoices above
         payments = Payment.query.filter_by(policy_id=self.policy.id)\
                                 .filter(Payment.transaction_date <= date_cursor)\
                                 .all()
 
-        if(len(payments) != 0): # Same concept as above
+        if(len(payments) != 0): # Same logic as above length of invoices above
+            print len(payments), " payments have been made. Listing..."
             for payment in payments:
-                due_now -= payment.amount_paid
-                print("Payment: ", payment.amount_paid)
-        
-        if(len(invoices) == 0 and len(payments) == 0): # If there are no payments or invoices, the account balance has not been paid
-            due_now = self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule)
+                print "A payment has been made in the amount $", payment.amount_paid
+                due_now -= payment.amount_paid # Subtract each payment from due_now
 
-        print("End of method\n")
+            print "The current amount due is now $", due_now, "\n"
+        else:
+            print "No payments have been made."
+
+        if(len(invoices) == 0 and len(payments) == 0): # If there are no payments or invoices, the account balance has not been paid
+            # If that's the case, set due_now equal to the annual premium divided by the billing schedule
+            due_now = self.policy.annual_premium / billing_schedules.get(self.policy.billing_schedule) #
+            print "Since no invoices are present and as such, no payments have been made, the current amount due is $", due_now, "\n"
+
         return due_now
 
     def make_payment(self, contact_id=None, date_cursor=None, amount=0):
-        if not date_cursor:
+        if not date_cursor: # If date_cursor is equal to the default value (None) or null, set it equal to the current date
             date_cursor = datetime.now().date()
 
-        if not contact_id:
+        if not contact_id: # If there is no contact_id or it is equal to the default value (None), set it equal to the named insured of the policy
             try:
                 contact_id = self.policy.named_insured
             except:
                 pass
 
-        payment = Payment(self.policy.id,
+        payment = Payment(self.policy.id, # Create a payment using the method parameters
                           contact_id,
                           amount,
                           date_cursor)
-        db.session.add(payment)
-        db.session.commit()
+        db.session.add(payment) # Add the payment to the database
+        db.session.commit() # Commit the changes (save)
 
         return payment
 
@@ -88,45 +112,50 @@ class PolicyAccounting(object):
         pass
 
     def evaluate_cancel(self, date_cursor=None):
-        if not date_cursor:
+        if not date_cursor: # If date_cursor is equal to the default value (None) or null, set it equal to the current date
             date_cursor = datetime.now().date()
 
+        # Access the database to grab invoices according to the current policy. Filter them by looking for all invoices whose billing date is on or before 
+        # the date_cursor, then order them by the billing date. The .all() method returns all matched invoices.
         invoices = Invoice.query.filter_by(policy_id=self.policy.id)\
                                 .filter(Invoice.cancel_date <= date_cursor)\
                                 .order_by(Invoice.bill_date)\
                                 .all()
 
-        for invoice in invoices:
-            if not self.return_account_balance(invoice.cancel_date):
-                continue
+        for invoice in invoices: # For each invoice...
+            if not self.return_account_balance(invoice.cancel_date): # If there is no account balance on the cancel date...
+                continue # Continue on to the next iteration of the for loop
             else:
-                print "THIS POLICY SHOULD HAVE CANCELED"
+                print "THIS POLICY SHOULD HAVE CANCELED" # Otherwise the policy should be cancelled
                 break
         else:
-            print "THIS POLICY SHOULD NOT CANCEL"
+            print "THIS POLICY SHOULD NOT CANCEL" # This is where you end up if invoices returns continue
 
 
     def make_invoices(self):
-        for invoice in self.policy.invoices: #figure out what this does
+        for invoice in self.policy.invoices: # If there are any invoices, delete them as we are making new ones
             invoice.delete()
 
         billing_schedules = {'Annual': None, 'Semi-Annual': 3, 'Quarterly': 4, 'Monthly': 12}
 
         invoices = []
 
-        first_invoice = Invoice(self.policy.id,
-                                self.policy.effective_date, #bill_date
-                                self.policy.effective_date + relativedelta(months=1), #due
-                                self.policy.effective_date + relativedelta(months=1, days=14), #cancel
+        first_invoice = Invoice(self.policy.id, # Create an invoice based on the policy
+                                self.policy.effective_date, # bill_date
+                                self.policy.effective_date + relativedelta(months=1), # due_date
+                                self.policy.effective_date + relativedelta(months=1, days=14), # cancel_date
                                 self.policy.annual_premium)
-        invoices.append(first_invoice)
+
+        invoices.append(first_invoice) # Add first_invoice to the invoices[] array
 
         if self.policy.billing_schedule == "Annual":
-            pass
+            pass # An annual billing policy allows us to simply use the above first_invoice because the values don't change
         elif self.policy.billing_schedule == "Two-Pay":
+            # Set the invoice amount equal to itself divided by the billing schedule
             first_invoice.amount_due = first_invoice.amount_due / billing_schedules.get(self.policy.billing_schedule)
 
-            for i in range(1, billing_schedules.get(self.policy.billing_schedule)):
+            for i in range(1, billing_schedules.get(self.policy.billing_schedule)): # For each bill in the billing schedule...
+                # Based on the billing schedule, get the next time the customer has to pay and set that as the billing date. 
                 months_after_eff_date = i*6
 
                 bill_date = self.policy.effective_date + relativedelta(months=months_after_eff_date)
@@ -171,10 +200,10 @@ class PolicyAccounting(object):
         else:
             print "You have chosen a bad billing schedule."
 
-        for invoice in invoices:
+        for invoice in invoices: # For every invoice created, add it to the database.
             db.session.add(invoice)
             
-        db.session.commit()
+        db.session.commit() # Commit the changes (save)
 
 ################################
 # The functions below are for the db and 
